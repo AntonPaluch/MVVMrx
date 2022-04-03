894a392a439d25948237b2a444a5e3d3c7ee7ed9
Dmitry Pavlov <perr07@yandex.ru>
2022-04-03 21:02:20 +0300

    Initial Commit

---
+++ MVVMrx/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVVMrx
+//
+//  Created by Pandos on 03.04.2022.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MVVMrx/Pods-MVVMrx-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVVMrx : NSObject
+@end
+@implementation PodsDummy_Pods_MVVMrx
+@end
